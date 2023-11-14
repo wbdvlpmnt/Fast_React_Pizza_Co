89a115bbf4c14130d24be6a20e0c1769852746d2
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-14 11:47:15 -0600

    strict mode for development mode

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
