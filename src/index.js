7080fcfafcde5c14efa46ed177409eeeff63758f
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-15 09:47:40 -0600

    A component can have two returns

--- src/index.js
+++ src/index.js
@@ -95,10 +95,17 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
+
+  if (!isOpen) {
+    return (
+      <p>
+        We're happy to welcome you between {openHour}:00 and {closeHour}:00
+      </p>
+    );
+  }
   // What it looks like without JSX
   //return React.createElement("footer", null, "We're currently open!");
   return (
